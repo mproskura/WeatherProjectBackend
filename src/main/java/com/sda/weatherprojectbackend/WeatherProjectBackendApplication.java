63e2e5fb3030db3b864094cd208db1a90cdcbd50
mproskura <mproskura@gmail.com>
2022-01-08 12:10:43 +0100

    Initial commit

---
+++ src/main/java/com/sda/weatherprojectbackend/WeatherProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sda.weatherprojectbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherProjectBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherProjectBackendApplication.class, args);
+    }
+
+}
